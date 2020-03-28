081664272339f52f2bb110afff767eb340d6fc21
Ahmad Farhat <rappar.stand@gmail.com>
2020-03-28 17:47:24 +0600

    basic stock added

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Stocks stock</div>
+  <b-col sm="12" md="6" style="padding:30px">
+    <b-card header="name" header-tag="header" title="Title">
+      <b-input type="number" placeholder="Quantity" class="mb-2 d-inline"></b-input>
+      <b-button href="#" variant="primary">Hello</b-button>
+    </b-card>
+  </b-col>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h2>stocks stocks</h2>
-  </div>
+  <b-row>
+    <Stock v-for="stock in stocks" v-bind:key="stock.id">{{stock.name}}</Stock>
+  </b-row>
 </template>
+<script>
+import Stock from "./Stock.vue";
+export default {
+  data: () => ({
+    stocks: [
+      { id: 1, name: "AH", price: 850 },
+      { id: 2, name: "AF", price: 550 },
+      { id: 3, name: "HAT", price: 750 },
+      { id: 4, name: "AT", price: 450 },
+      { id: 5, name: "AH", price: 850 }
+    ]
+  }),
+  components: {
+    Stock
+  }
+};
+</script>
