f06cb279089cebb1411aa9ae87e5bf72abd6913e
Ahmad Farhat <rappar.stand@gmail.com>
2020-04-01 17:46:36 +0600

    animation added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <b-container fluid id="app">
     <Header></Header>
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </b-container>
 </template>
 
@@ -18,7 +20,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +29,34 @@ body {
   color: #2c3e50;
   padding: 30px;
   box-sizing: border-box;
+}
+.slide {
+  &-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+  &-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
